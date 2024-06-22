7669975cb4b2fd3ec40f20f6ee411addd605d679
Dasajton <david.sajitz.94@gmail.com>
2024-06-23 01:15:23 +0200

    modal function templates unfinished

--- src/components/ModalCreateBlog/ModalCreateBlog.tsx
+++ src/components/ModalCreateBlog/ModalCreateBlog.tsx
@@ -3,15 +3,24 @@ import { useBlogContext } from "../../BlogContext";
 
 export default function ModalCreateBlog() {
   const { modalCreateOpen, setModalCreateOpen } = useBlogContext();
+
+  const handleCloseModal = () => {
+    setModalCreateOpen(false);
+  };
+
+  const handleCreateBlog = () => {};
+
   return (
     <div className="overlay">
       <div className={`modalContainer ${modalCreateOpen ? "scaleUp" : ""}`}>
         <header className="modalHeader">
           <h2 className="modalHeadline">Create new Blog Entry</h2>
-          <button className="btnCloseModal">Close</button>
+          <button className="btnCloseModal" onClick={handleCloseModal}>
+            Close
+          </button>
         </header>
 
-        <form className="modalForm">
+        <form onSubmit={handleCreateBlog} className="modalForm">
           <input type="text" placeholder="Title" className="modalTitle" />
           <input type="date" placeholder="Date" className="modalDate" />
           <textarea placeholder="Description" className="modalBody"></textarea>
