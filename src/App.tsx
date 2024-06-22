d1e07d3341c198e3ac32bcf72d2cb55a637430ef
Dasajton <david.sajitz.94@gmail.com>
2024-06-23 01:11:38 +0200

    modal mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Menubar from "./components/Menubar/Menubar";
 import BlogEntries from "./components/BlogEntries/BlogEntries";
+import { useBlogContext } from "./BlogContext";
+import ModalCreateBlog from "./components/ModalCreateBlog/ModalCreateBlog";
 
 export default function App() {
+  const { modalCreateOpen } = useBlogContext();
   return (
     <div>
       <Menubar />
       <BlogEntries />
+      {modalCreateOpen && <ModalCreateBlog />}
     </div>
   );
 }
--- src/components/ModalCreateBlog/ModalCreateBlog.tsx
+++ src/components/ModalCreateBlog/ModalCreateBlog.tsx
@@ -1,5 +1,23 @@
 import "./ModalCreateBlog.scss";
+import { useBlogContext } from "../../BlogContext";
 
 export default function ModalCreateBlog() {
-  return <div>ModalCreateBlog</div>;
+  const { modalCreateOpen, setModalCreateOpen } = useBlogContext();
+  return (
+    <div className="overlay">
+      <div className={`modalContainer ${modalCreateOpen ? "scaleUp" : ""}`}>
+        <header className="modalHeader">
+          <h2 className="modalHeadline">Create new Blog Entry</h2>
+          <button className="btnCloseModal">Close</button>
+        </header>
+
+        <form className="modalForm">
+          <input type="text" placeholder="Title" className="modalTitle" />
+          <input type="date" placeholder="Date" className="modalDate" />
+          <textarea placeholder="Description" className="modalBody"></textarea>
+          <button className="modalBtnCreate">Create Blog</button>
+        </form>
+      </div>
+    </div>
+  );
 }
